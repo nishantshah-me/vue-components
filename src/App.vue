d5f6f56daa262e4268211b95d6b9607d026bd41b
Nishant Shah <nishantcshah13@gmail.com>
2023-02-23 10:59:05 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png">
+
 </template>
 
 <script>
@@ -10,7 +11,7 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+    }
 }
 </script>
 
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
